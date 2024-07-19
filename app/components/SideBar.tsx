0efea2d1067aacb31f2c29afbe1e4dbd9f2e38c3
bao-tranc-otsv <bao.tranc@one-line.com>
2024-07-19 20:16:33 +0700

    SideBar

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -2,7 +2,6 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
 import ListItemText from "@mui/material/ListItemText";
 import { Drawer } from "@mui/material";
 import SchoolIcon from '@mui/icons-material/School';
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {SideBar} from "@/app/components/SideBar";
+import { SideBar } from "@/app/components/SideBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,15 +16,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <html lang="en">
+    <html lang="en">
       <body className={inter.className}>
-      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
           <div className="w-full flex-none md:w-64">
-              <SideBar/>
+            <SideBar />
           </div>
-          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
-      </div>
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+            {children}
+          </div>
+        </div>
       </body>
-      </html>
+    </html>
   );
 }
