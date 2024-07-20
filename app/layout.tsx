be23420dc5790990c817dc192124eeabe1b54aa8
bao-tranc-otsv <bao.tranc@one-line.com>
2024-07-20 16:14:05 +0700

    cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import SideBar from "@/app/components/SideBar";
+import "./decks/index.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex">
-          <SideBar />
-          {children}
-        </div>
+        <SideBar />
+        {children}
       </body>
     </html>
   );
