4e080e3ce9c289d5e4a1897056fbc9471ebf6db0
bao-tranc-otsv <bao.tranc@one-line.com>
2024-07-20 15:58:30 +0700

    cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,9 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
           <SideBar />
-          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-            {children}
-          </div>
+          {children}
         </div>
       </body>
     </html>
