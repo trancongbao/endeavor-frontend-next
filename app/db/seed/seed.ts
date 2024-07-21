9b91e8de8f7b34b2f16a7380c4cdb87a0d4b70e3
bao-tranc-otsv <bao.tranc@one-line.com>
2024-07-21 11:23:22 +0700

    seeding

--- app/db/seed/seed.ts
+++ app/db/seed/seed.ts
@@ -1,33 +1,62 @@
-import { Insertable } from "kysely";
-import { Course, kysely, Lesson } from "../kysely";
+import { Insertable, sql } from "kysely";
+import { Course, EndeavorDB, kysely, Lesson, Teacher } from "../kysely";
 import { teachers, courses } from "./data";
 
-//kysely.insertInto("teacher").values(teachers).execute();
-
-const courseInsertables: Insertable<Course>[] = courses.map(
-  ({ lessons, ...courseInsertable }) => courseInsertable
-);
-insertCourses(courseInsertables).then((insertedCourses) => {
-  console.log("Course: ", insertedCourses);
-  const courseIds = insertedCourses.map((course) => course.id);
-  console.log("Course ids: ", courseIds);
-
-  courses.forEach((course, index) => {
-    course.id = courseIds[index];
-    course.lessons?.forEach((lesson, index) => {
-      lesson.course_id = course.id;
-      lesson.lesson_order = index;
-    });
-  });
-  console.log("Updated courses: ", courses);
-  
-  const lessonInsertables = courses.map((course) => course.lessons).flat().filter(lessons => lessons !== undefined) as Insertable<Lesson>[];
-  console.log("lessonInsertables: ", lessonInsertables);
-
-  insertLessons(lessonInsertables).then(insertedLessons => {
-    console.log("Inserted lessons: ", insertedLessons)
+truncateTables()
+  .then(() => {
+    return insertTeachers(teachers);
   })
-});
+  .then(() => {
+    const courseInsertables: Insertable<Course>[] = courses.map(
+      ({ lessons, ...courseInsertable }) => courseInsertable
+    );
+
+    return insertCourses(courseInsertables);
+  })
+  .then((insertedCourses) => {
+    console.log("Courses: ", insertedCourses);
+    const courseIds = insertedCourses.map((course) => course.id);
+    console.log("Course ids: ", courseIds);
+
+    courses.forEach((course, index) => {
+      course.id = courseIds[index];
+      course.lessons?.forEach((lesson, lessonIndex) => {
+        lesson.course_id = course.id;
+        lesson.lesson_order = lessonIndex;
+      });
+    });
+
+    console.log("Updated courses: ", courses);
+
+    const lessonInsertables = courses
+      .map((course) => course.lessons)
+      .flat()
+      .filter((lesson) => lesson !== undefined) as Insertable<Lesson>[];
+
+    console.log("lessonInsertables: ", lessonInsertables);
+
+    return insertLessons(lessonInsertables);
+  })
+  .then((insertedLessons) => {
+    console.log("Inserted lessons: ", insertedLessons);
+  });
+
+function truncateTables() {
+  const snippet = sql`
+    TRUNCATE TABLE lesson CASCADE;
+    TRUNCATE TABLE course CASCADE;
+    TRUNCATE TABLE teacher CASCADE;
+  `;
+  return snippet.execute(kysely);
+}
+
+function insertTeachers(teacherInsertables: Insertable<Teacher>[]) {
+  return kysely
+    .insertInto("teacher")
+    .values(teacherInsertables)
+    .returningAll()
+    .execute();
+}
 
 function insertCourses(courseInsertables: Insertable<Course>[]) {
   return kysely
