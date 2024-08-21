4e0b6e78329c8594650a2418bdf8c45084ff1918
bao-tranc-otsv <bao.tranc@one-line.com>
2024-08-21 14:40:56 +0700

    SideBar

--- app/components/SideBar/Profile/Profile.jsx
+++ app/components/SideBar/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import './Profile.scss';
-import { rpc } from '../../../rpc/rpc';
+// import { rpc } from '../../../rpc/rpc';
 
 export default function Profile({ authenticatedUser, setAuthenticatedUser }) {
   return (
@@ -9,9 +9,9 @@ export default function Profile({ authenticatedUser, setAuthenticatedUser }) {
         src="https://i.seadn.io/gae/IZEEg3gIDw-zlV7Z8l1_-OgEkYKnFXJXYjEKY6Ue-Nuhfk-cv0FnRA5eOC2lM8EdEHKHNWljh_7pUmxXNtKKlzqqeZwWmNhGCoCMtew?auto=format&dpr=1&w=1000"
         alt=""
       />
-      <h3>{authenticatedUser.username}</h3>
-      <div>{authenticatedUser.userType.toUpperCase()}</div>
-      {authenticatedUser.level && <div>Level: ${authenticatedUser.level}</div>}
+      <h3>authenticatedUser.username</h3>
+      <div>authenticatedUser.userType.toUpperCase()</div>
+      {authenticatedUser?.level && <div>Level: ${authenticatedUser.level}</div>}
 
       <div className="flex-btn">
         <div className="option-btn" onClick={profile}>
@@ -25,13 +25,13 @@ export default function Profile({ authenticatedUser, setAuthenticatedUser }) {
   );
 
   function logout() {
-    rpc('auth', 'logout', {}).then((result) => {
-      if (result) {
-        setAuthenticatedUser({});
-      } else {
-        alert('Logout error.');
-      }
-    });
+    // rpc('auth', 'logout', {}).then((result) => {
+    //   if (result) {
+    //     setAuthenticatedUser({});
+    //   } else {
+    //     alert('Logout error.');
+    //   }
+    // });
   }
 
   function profile() {
---
+++ app/components/SideBar/SideBar.tsx
@@ -0,0 +1,17 @@
+import './SideBar.scss'
+import Logo from './Logo/Logo'
+import Profile from './Profile/Profile';
+import Menu from './Menu/Menu'
+
+export default function SideBar({ authenticatedUser, setAuthenticatedUser }) {
+  return (
+    <div className="side-bar active">
+      <Logo />
+      <Profile
+        authenticatedUser={authenticatedUser}
+        setAuthenticatedUser={setAuthenticatedUser}
+      />
+      <Menu />
+    </div>
+  )
+}
