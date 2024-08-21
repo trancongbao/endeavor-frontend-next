781dafb1e3737c2c47e143a73bf7a9736eeed4e9
bao-tranc-otsv <bao.tranc@one-line.com>
2024-08-21 16:39:49 +0700

    menu

--- app/components/SideBar/Menu/Menu.jsx
+++ app/components/SideBar/Menu/Menu.jsx
@@ -2,7 +2,7 @@
 
 const Menu = () => {
   return (
-    <nav className="flex flex-col">
+    <nav className="flex flex-col gap-4 text-2xl">
       <a href='/decks'>My Courses</a>
       <a href='/decks'>My Decks</a>
       <a href='/decks'>Contact us</a>
--- app/components/SideBar/Profile/Profile.jsx
+++ app/components/SideBar/Profile/Profile.jsx
@@ -3,9 +3,17 @@ import Image from 'next/image'
 // import { rpc } from '../../../rpc/rpc';
 
 export default function Profile({ authenticatedUser, setAuthenticatedUser }) {
+  const buttonClass = 'w-52 h-16 rounded-lg bg-orange-300 flex justify-center items-center text-white text-2xl'
+
   return (
     <div className="profile active flex flex-col items-center gap-3">
-      <Image className="rounded-full border border-zinc-950" src="/sidebar/avatar.webp" alt="" width={100} height={100} />
+      <Image
+        className="rounded-full border border-zinc-950"
+        src="/sidebar/avatar.webp"
+        alt=""
+        width={100}
+        height={100}
+      />
       {/* <h3>{authenticatedUser.username}</h3> */}
       <p className="text-3xl">Tran Cong Bao</p>
       {/* <div>{authenticatedUser.userType.toUpperCase()}</div> */}
@@ -13,16 +21,10 @@ export default function Profile({ authenticatedUser, setAuthenticatedUser }) {
       {authenticatedUser?.level && <div>Level: ${authenticatedUser.level}</div>}
 
       <div className="flex items-center gap-4">
-        <button
-          className="w-52 h-16 rounded-lg bg-orange-300 flex justify-center items-center text-white text-2xl"
-          onClick={profile}
-        >
+        <button className={buttonClass} onClick={profile}>
           Profile
         </button>
-        <button
-          className="w-52 h-16 rounded-lg bg-orange-300 flex justify-center items-center text-white text-2xl"
-          onClick={logout}
-        >
+        <button className={buttonClass} onClick={logout}>
           Logout
         </button>
       </div>
