fbf2dae47dc0ba12b7bce5ede3a1a7f24178175b
bao-tranc-otsv <bao.tranc@one-line.com>
2024-08-21 16:40:34 +0700

    menu

--- app/components/SideBar/Menu/Menu.jsx
+++ app/components/SideBar/Menu/Menu.jsx
@@ -7,42 +7,6 @@ const Menu = () => {
       <a href='/decks'>My Decks</a>
       <a href='/decks'>Contact us</a>
     </nav>
-
-    // <nav className="navbar">
-    //   <NavLink
-    //     className={({ isActive, isPending }) =>
-    //       isActive ? 'active' : isPending ? 'pending' : ''
-    //     }
-    //     to={``}
-    //   >
-    //     <div>
-    //       <i className="fa fa-graduation-cap" />
-    //       <span>My Courses</span>
-    //     </div>
-    //   </NavLink>
-    //   <NavLink
-    //     className={({ isActive, isPending }) =>
-    //       isActive ? 'active' : isPending ? 'pending' : ''
-    //     }
-    //     to={``}
-    //   >
-    //     <div>
-    //       <i className="fa fa-book"></i>
-    //       <span>My Decks</span>
-    //     </div>
-    //   </NavLink>
-    //   <NavLink
-    //     className={({ isActive, isPending }) =>
-    //       isActive ? 'active' : isPending ? 'pending' : ''
-    //     }
-    //     to={``}
-    //   >
-    //     <div>
-    //       <i className="fa fa-phone" />
-    //       <span>Contact us</span>
-    //     </div>
-    //   </NavLink>
-    // </nav>
   )
 }
 export default Menu
