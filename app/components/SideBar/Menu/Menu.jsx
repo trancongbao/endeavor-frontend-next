e7ec83a7b14468d6df2c497bacc52894b1aa3d3c
bao-tranc-otsv <bao.tranc@one-line.com>
2024-08-23 10:52:11 +0700

    Link

--- app/components/SideBar/Menu/Menu.jsx
+++ app/components/SideBar/Menu/Menu.jsx
@@ -1,19 +1,20 @@
 import { Book, Layers, Phone } from 'react-feather'
+import Link from 'next/link'
 
 const items = [
-  { text: "My Courses", icon: Book, href: "/decks" },
-  { text: "My Decks", icon: Layers, href: "/decks" },
-  { text: "Contact Us", icon: Phone, href: "/decks" },
+  { text: 'My Courses', icon: Book, href: '/decks' },
+  { text: 'My Decks', icon: Layers, href: '/decks' },
+  { text: 'Contact Us', icon: Phone, href: '/decks' },
 ]
 
 export default function Menu() {
   return (
     <nav className="self-start p-10 flex flex-col gap-7 text-[18px]">
       {items.map((item, index) => (
-        <a href={item.href} key={index} className="flex gap-3 items-center">
+        <Link href={item.href} key={index} className="flex gap-3 items-center">
           <item.icon color="hsl(180, 70%, 50%)" />
           {item.text}
-        </a>
+        </Link>
       ))}
     </nav>
   )
