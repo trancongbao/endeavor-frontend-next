bd58113f8c102c660ef44e18965318321e3dde0f
bao-tranc-otsv <bao.tranc@one-line.com>
2024-08-26 15:32:24 +0700

    Preview - back

--- app/teacher/decks/[id]/Browser/Preview.tsx
+++ app/teacher/decks/[id]/Browser/Preview.tsx
@@ -8,17 +8,16 @@ export default function Preview({ card }: { card: Row[] }) {
       <p
         className="self-center text-center text-lg"
         dangerouslySetInnerHTML={{
-          __html: card ? styleNewWord(card[0].cardText) : '',
+          __html: styleNewWord(card[0].cardText),
         }}
       ></p>
       <hr></hr>
-      {card &&
-        card.map((word, index) => (
-          <div key={index} className="">
-            <span className="">{word.wordText} </span>
-            <span className="">:: {word.wordDefinition}</span>
-          </div>
-        ))}
+      {card.map((word, index) => (
+        <div key={index} className="">
+          <span className="font-bold text-primary-600">{word.wordText}</span>
+          <span className=""> :: {word.wordDefinition}</span>
+        </div>
+      ))}
     </div>
   )
 }
