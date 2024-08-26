565132d9830580682c2464a98d5d13d3cf9d1c48
bao-tranc-otsv <bao.tranc@one-line.com>
2024-08-26 17:05:30 +0700

    Preview/Edit Toggle

--- app/teacher/decks/[id]/Browser/Toggle.tsx
+++ app/teacher/decks/[id]/Browser/Toggle.tsx
@@ -8,15 +8,18 @@ interface ToggleSwitchProps {
 const ToggleSwitch: React.FC<ToggleSwitchProps> = ({ isChecked, onChange }) => {
   const [checked, setChecked] = useState(isChecked)
 
-  const handleToggle = () => {
-    setChecked(!checked)
-    onChange(!checked)
-  }
-
   return (
     <div className="w-52 h-10 bg-gray-100 border border-gray-300 rounded-md grid grid-cols-2 items-center">
-      <button className={`text-center ${checked ? 'bg-none text-gray-400' : 'text-white'}`}>Preview</button>
-      <button className={` text-gray-400 text-center  ${checked ? 'bg-orange-300 text-white' : 'text-white'}`}>
+      <button
+        className={`h-5/6 ml-1 ${checked ? 'text-gray-500' : 'bg-orange-400 text-white'}`}
+        onClick={() => setChecked(false)}
+      >
+        Preview
+      </button>
+      <button
+        className={`h-5/6 mr-1 ${checked ? 'bg-orange-400 text-white' : 'text-gray-500'}`}
+        onClick={() => setChecked(true)}
+      >
         Edit
       </button>
     </div>
