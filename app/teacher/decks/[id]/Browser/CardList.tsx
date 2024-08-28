e75db68b88171610f82924b4e41819109903c7f8
bao-tranc-otsv <bao.tranc@one-line.com>
2024-08-28 12:06:38 +0700

    Fix selectedCardRows

--- app/teacher/decks/[id]/Browser/CardList.tsx
+++ app/teacher/decks/[id]/Browser/CardList.tsx
@@ -25,7 +25,7 @@ export default function CardList({ selectedSubdeckRows }: { selectedSubdeckRows:
           <ul className="flex flex-col gap-4">
             {Object.keys(groupedCardRows).map((cardOrder) => (
               <li
-                className={`p-2 rounded cursor-pointer  hover:bg-orange-50 ${selectedCardRows.length > 0 && parseInt(cardOrder) === selectedCardRows[0].cardOrder ? 'bg-orange-200' : ''}`}
+                className={`p-2 rounded cursor-pointer  hover:bg-orange-50 ${selectedCardRows.length > 0 && selectedCardRows[0].cardOrder === parseInt(cardOrder) ? 'bg-orange-200' : ''}`}
                 key={cardOrder}
                 onClick={() => setSelectedCardRows(groupedCardRows[cardOrder])}
                 dangerouslySetInnerHTML={{
