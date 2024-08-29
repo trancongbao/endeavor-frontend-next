7152f7da0606cf192c6204dc4e222862b6de427e
bao-tranc-otsv <bao.tranc@one-line.com>
2024-08-29 17:35:00 +0700

    editSubdeckTitle

--- app/actions.ts
+++ app/actions.ts
@@ -31,9 +31,17 @@ export async function deleteSubdeck(courseId: number, subdeckOrder: number) {
 
 export async function editSubdeckTitle(formData: FormData) {
   console.log('editSubdeckTitle: formData = ', formData)
-  // const deletedSubdeck = await kysely.deleteFrom('lesson').returningAll().executeTakeFirstOrThrow()
-  // console.log('Added subdeck: ', deletedSubdeck)
-  // revalidatePath('/teacher/decks/[id]')
+  const updatedSubdeck = await kysely
+    .updateTable('lesson')
+    .where('course_id', '=', parseInt(formData.get('courseId') as string))
+    .where('order', '=', parseInt(formData.get('order') as string))
+    .set({
+      title: formData.get('title') as string,
+    })
+    .returningAll()
+    .executeTakeFirstOrThrow()
+  console.log('Updated subdeck: ', updatedSubdeck)
+  revalidatePath('/teacher/decks/[id]')
 }
 
 export async function addCard(formData: FormData) {
--- app/teacher/decks/[id]/Browser/Subdeck.tsx
+++ app/teacher/decks/[id]/Browser/Subdeck.tsx
@@ -35,14 +35,13 @@ export default function Subdeck({
           <Menu onSelect={onSelect} />
         </div>
       ) : (
-        <div className='flex'>
-          <form action={editSubdeckTitle} onSubmit={() => setIsEditingSubdeckTitle(false)} className='flex-1'>
+        <div className="flex">
+          <form action={editSubdeckTitle} onSubmit={() => setIsEditingSubdeckTitle(false)} className="flex-1">
             <Input type="hidden" name="courseId" value={courseId} />
             <Input type="hidden" name="order" value={subdeckOrder} />
             <Input
               name="title"
               ref={editSubdeckTitileInputRef}
-              placeholder="Edit the subdeck title and press Return."
               value={newSubdeckTitle}
               onChange={(e) => setNewSubdeckTitle(e.target.value)}
             />
@@ -50,7 +49,10 @@ export default function Subdeck({
           <Button
             variant="outline"
             className="w-20 bg-orange-400  text-white text-md hover:bg-orange-300 hover:text-black py-2 px-4 rounded"
-            onClick={() => setIsEditingSubdeckTitle(false)}
+            onClick={() => {
+              setIsEditingSubdeckTitle(false)
+              setNewSubdeckTitle(subdeckTitle)
+            }}
           >
             Cancel
           </Button>
@@ -64,8 +66,12 @@ export default function Subdeck({
     switch (action) {
       case 'edit':
         setIsEditingSubdeckTitle(true)
+        break
       case 'delete':
         deleteSubdeck(courseId, subdeckOrder)
+        break
+      default:
+        break
     }
   }
 }
