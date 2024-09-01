8411545afb48821f40e77d60d5cf4ae18e0ec632
bao-tranc-otsv <bao.tranc@one-line.com>
2024-09-01 17:27:10 +0700

    /api/word

--- app/api/word/route.ts
+++ app/api/word/route.ts
@@ -1,13 +1,32 @@
+import { kysely } from '@/app/db/kysely'
+import { sql } from 'kysely'
 import { type NextRequest } from 'next/server'
 
-export function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
   const query = searchParams.get('query')
+
+  const response = await kysely
+    .selectFrom('word')
+    .selectAll()
+    .where(sql`to_tsvector(word)`, '@@', sql`to_tsquery(${query})`)
+    .execute()
+
+  console.log('response: ', response)
 
   return Response.json([
     {
       id: 1,
-      text: 'text',
+      text: 'word1',
+      definition: 'definition',
+      phonetic: 'phonetic',
+      part_of_speech: 'part_of_speech',
+      audio_uri: 'audio_uri',
+      image_uri: 'image_uri',
+    },
+    {
+      id: 1,
+      text: 'word2',
       definition: 'definition',
       phonetic: 'phonetic',
       part_of_speech: 'part_of_speech',
