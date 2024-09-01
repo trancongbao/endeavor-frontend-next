fe9c1612ce34324a0cb6ef441701ea2e90191219
bao-tranc-otsv <bao.tranc@one-line.com>
2024-09-01 17:58:59 +0700

    /api/word

--- app/api/word/route.ts
+++ app/api/word/route.ts
@@ -3,35 +3,11 @@ import { sql } from 'kysely'
 import { type NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams
-  const query = searchParams.get('query')
-
   const response = await kysely
     .selectFrom('word')
     .selectAll()
-    .where(sql`to_tsvector(text)`, '@@', sql`to_tsquery(${query})`)
+    .where(sql`to_tsvector(text)`, '@@', sql`to_tsquery(${request.nextUrl.searchParams.get('query')})`)
     .execute()
 
-  console.log('response: ', response)
-
-  return Response.json([
-    {
-      id: 1,
-      text: 'word1',
-      definition: 'definition',
-      phonetic: 'phonetic',
-      part_of_speech: 'part_of_speech',
-      audio_uri: 'audio_uri',
-      image_uri: 'image_uri',
-    },
-    {
-      id: 1,
-      text: 'word2',
-      definition: 'definition',
-      phonetic: 'phonetic',
-      part_of_speech: 'part_of_speech',
-      audio_uri: 'audio_uri',
-      image_uri: 'image_uri',
-    },
-  ])
+  return Response.json(response)
 }
