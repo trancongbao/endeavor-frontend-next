a110e1f7e7bbdc246233580cb6339341fe4fb7d6
bao-tranc-otsv <bao.tranc@one-line.com>
2024-09-03 20:59:22 +0700

    highlightTargetWords

--- app/teacher/decks/[id]/Browser/highlightTargetWords.tsx
+++ app/teacher/decks/[id]/Browser/highlightTargetWords.tsx
@@ -1,6 +1,8 @@
 export function highlightTargetWords(cardText: string, targetWordPositions: { start: number; end: number }[]): string {
+  const bTag = '<b class="text-primary-600">'
   // Sort positions by start index to handle inserts correctly
   const sortedPositions = targetWordPositions.sort((a, b) => a.start - b.start)
+  console.log(`highlightTargetWords: "sortedPositions=${sortedPositions}`)
 
   // Offset to keep track of the shift in indices after insertion
   let offset = 0
@@ -10,14 +12,14 @@ export function highlightTargetWords(cardText: string, targetWordPositions: { st
     const endIndex = position.end + offset
 
     // Insert opening <b> tag
-    cardText = cardText.slice(0, startIndex) + '<b>' + cardText.slice(startIndex)
-    offset += 3 // Length of "<b>" is 3
+    cardText = cardText.slice(0, startIndex) + bTag + cardText.slice(startIndex)
+    offset += 28 // Length of "<b class="text-primary-600">" is 3
 
     // Insert closing </b> tag
     cardText =
-      cardText.slice(0, endIndex + 3) + // +3 to account for the added <b> tag
+      cardText.slice(0, endIndex + 28) + // +3 to account for the added <b> tag
       '</b>' +
-      cardText.slice(endIndex + 3)
+      cardText.slice(endIndex + 28)
     offset += 4 // Length of "</b>" is 4
   }
 
