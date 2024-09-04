ba3e5be868987375cb2cbe0fc15ac301d6912a5c
bao-tranc-otsv <bao.tranc@one-line.com>
2024-09-04 09:35:47 +0700

    highlightTargetWords

--- app/teacher/decks/[id]/Browser/highlightTargetWords.tsx
+++ app/teacher/decks/[id]/Browser/highlightTargetWords.tsx
@@ -12,13 +12,13 @@ export function highlightTargetWords(cardText: string, targetWordPositions: { st
     const startIndex = position.start + offset
     const endIndex = position.end + offset
 
-    // Insert opening <b> tag
+    // Insert opening <b> opening tag
     cardText = cardText.slice(0, startIndex) + bTagOpening + cardText.slice(startIndex)
     offset += bTagOpening.length
 
     // Insert closing </b> tag
     cardText =
-      cardText.slice(0, endIndex + bTagOpening.length) + bTagClosing + cardText.slice(endIndex + bTagOpening.length)
+      cardText.slice(0, endIndex + bTagOpening.length + 1) + bTagClosing + cardText.slice(endIndex + bTagOpening.length + 1)
     offset += bTagClosing.length
   }
 
