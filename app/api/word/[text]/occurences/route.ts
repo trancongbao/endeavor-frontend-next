b0b50127b40f4340c6c8f1a5d44e32b6d9bd0cb6
bao-tranc-otsv <bao.tranc@one-line.com>
2024-09-11 15:45:10 +0700

    /api/word/[text]/occurences

--- app/api/word/[text]/occurences/route.ts
+++ app/api/word/[text]/occurences/route.ts
@@ -5,9 +5,9 @@ import { type NextRequest } from 'next/server'
 export async function GET(request: NextRequest, { params }: { params: { text: string } }) {
   console.log('params: ', params)
   const response = await kysely
-    .selectFrom('word')
+    .selectFrom('lesson')
     .selectAll()
-    .where(sql`to_tsvector(text)`, '@@', sql`to_tsquery(${params.text})`)
+    .where(sql`to_tsvector(content)`, '@@', sql`to_tsquery(${params.text})`)
     .execute()
 
   return Response.json(response)
