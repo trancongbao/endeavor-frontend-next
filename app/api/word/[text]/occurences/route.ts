12c8c595ab7d11ad23fd602885d0bb86eb55f8ac
bao-tranc-otsv <bao.tranc@one-line.com>
2024-09-11 17:11:47 +0700

    ShortWord=0 MinWords=16 MaxWords=20

--- app/api/word/[text]/occurences/route.ts
+++ app/api/word/[text]/occurences/route.ts
@@ -6,9 +6,14 @@ export async function GET(request: NextRequest, { params }: { params: { text: st
   console.log('params: ', params)
   const response = await kysely
     .selectFrom('lesson')
-    .select(sql`ts_headline(content, to_tsquery(${params.text}), 'StartSel=<b>, StopSel=</b>')`.as('snippet'))
+    .select(
+      sql`ts_headline('english', content, to_tsquery(${params.text}), 'StartSel=<b>, StopSel=</b> ShortWord=0 MinWords=16 MaxWords=20')`.as(
+        'snippet'
+      )
+    )
     .where(sql`to_tsvector(content)`, '@@', sql`to_tsquery(${params.text})`)
     .execute()
 
+  console.log('response: ', response)
   return Response.json(response)
 }
